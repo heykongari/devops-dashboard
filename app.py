a1b23df21cf78a420a195361cdfa5225a1041508
sagar kongari <sagarkongariwork@gmail.com>
2025-09-02 11:31:04 +0000

    demo app,py

--- app.py
+++ app.py
@@ -1,28 +1,9 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+app = Flask(__name__)
 
-db = SQLAlchemy()
-
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config.config')
-
-    db.init_app(app)
-
-    from routes.builds import builds_bp
-    from routes.deployments import deployments_bp
-    from routes.alerts import alerts_bp
-
-    app.register_blueprint(builds_bp, url_prefix='/builds')
-    app.register_blueprint(deployments_bp, url_prefix='/deployments')
-    app.register_blueprint(alerts_bp, url_prefix='/alerts')
-
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-    
-    return app
+@app.route('/')
+def demo():
+    return '<h1>Hello from Github!</h1>'
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
